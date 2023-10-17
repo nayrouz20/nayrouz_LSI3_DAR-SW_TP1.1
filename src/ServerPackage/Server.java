38aa46635d73fe719e1605f3ce0a43f609a8d404
nayrouz20 <nayrouzweslati2002@gmail.com>
2023-10-17 19:24:31 +0100

    second commit

---
+++ src/ServerPackage/Server.java
@@ -0,0 +1,16 @@
+package ServerPackage;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main (String[] args) throws IOException{
+        ServerSocket ss = new ServerSocket(2020);
+        System.out.println("Je suis un server en attente la connexion d'un client");
+        Socket s = ss.accept();
+        System.out.println("un client est connecté");
+
+        s.close();
+        ss.close();
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author lenovo
+ *
+ */
+module tp1_act11 {
+}
